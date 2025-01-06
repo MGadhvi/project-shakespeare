f8a700142ee655016a91f017fda50bf17c06b2b7
mgd <mgd@posteo.net>
2025-01-06 21:32:02 +0000

    added requirements.txt

---
+++ manage.py
@@ -0,0 +1,22 @@
+#!/gnu/store/k3irgi3kwws5m3r6jiyk2l8n4zyl99rj-python-wrapper-3.10.7/bin/python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
